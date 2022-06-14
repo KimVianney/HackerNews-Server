be66f1b0114b7654e96dc5141c3a08830bce7e65
KimVianney <kimvianney6@gmail.com>
2022-06-14 18:17:35 +0300

    Configure GraphQL interactive IDE and create new schema

--- links/models.py
+++ links/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.forms import DateField
 
 # Create your models here.
 class Link(models.Model):
     url = models.URLField()
     description = models.TextField(blank=True)
+   
 
